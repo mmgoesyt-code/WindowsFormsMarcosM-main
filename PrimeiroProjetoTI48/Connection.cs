aa02b781f392678e1fa1814edf21110a8fa9fd40
MARCOS MIGUEL GOES DA SILVA <marcos.mgsilva@senacsp.edu.br>
2026-02-23 16:50:18 -0300

    ADICIONADO NOVO

---
+++ PrimeiroProjetoTI48/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+public class Connection
+{
+    private SqlConnection conn;
+
+    string connectionString = @"Server=JUN0570952W10-1\BDSENAC; Database=BoaDB; User ID=senaclivre; Password=senaclivre;";
+
+    public SqlConnection Connect()
+    {
+        conn = new SqlConnection(connectionString);
+
+        if (conn.State == ConnectionState.Closed)
+            conn.Open();
+
+        return conn;
+    }
+
+    public void Disconnect()
+    {
+        if (conn != null && conn.State == ConnectionState.Open)
+            conn.Close();
+    }
+}
